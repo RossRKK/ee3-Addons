b58af4a401cfe11c3934d3685e1a2e0f9cc3228f
Ross Kenneth Keslo <rosskelso@btinternet.com>
2013-04-03 10:07:09 +0100

    Changed how the copper transmutation class works

--- common/com/github/RossRKK/EE3_Addons/recipes/CopperTransmute.java
+++ common/com/github/RossRKK/EE3_Addons/recipes/CopperTransmute.java
@@ -1,4 +1,5 @@
 package com.github.RossRKK.EE3_Addons.recipes;
+
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
@@ -10,29 +11,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+public class CopperTransmute {
+    private static ItemStack philStone = new ItemStack(ModItems.philStone, 1,
+            OreDictionary.WILDCARD_VALUE);
+    private static ItemStack miniumStone = new ItemStack(ModItems.miniumStone,
+            1, OreDictionary.WILDCARD_VALUE);
 
-public class CopperTransmute
-{
-    private static ItemStack philStone = new ItemStack(ModItems.philStone, 1, OreDictionary.WILDCARD_VALUE);
-    private static ItemStack miniumStone = new ItemStack(ModItems.miniumStone, 1, OreDictionary.WILDCARD_VALUE);
-    
-    public static List<ItemStack> transmutationStones = Arrays.asList(miniumStone, philStone);
-    
-    public static void init()
-    {
-        for (ItemStack stone : transmutationStones) 
-        {
+    public static List<ItemStack> transmutationStones = Arrays.asList(
+            miniumStone, philStone);
+
+    public static void init() {
+        for (ItemStack stone : transmutationStones) {
             copper(stone);
         }
     }
-    
-    public static void copper(ItemStack transmutationStone)
-    {   
-        try
-        {
-        ArrayList<ItemStack > copperOres = OreDictionary.getOres("ingotCopper");
-        ItemStack copperItem = copperOres.get(0);
-        RecipeHelper.addRecipe(copperItem, transmutationStone , Item.ingotIron, Item.ingotIron);
-        } catch (Exception e){/*Swallowed*/}
+
+    public static void copper(ItemStack transmutationStone) {
+        try {
+            ArrayList<ItemStack> copperOres = OreDictionary
+                    .getOres("ingotCopper");
+            ItemStack copperItem = copperOres.get(0);
+            RecipeHelper.addRecipe(copperItem, transmutationStone,
+                    Item.ingotIron, Item.ingotIron);
+        } catch (Exception e) {/* Swallowed */}
     }
 }
