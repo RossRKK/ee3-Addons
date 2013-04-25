3222312f09b0b13234591a30beb19b0879850042
Ross Kenneth Keslo <rosskelso@btinternet.com>
2013-04-25 21:26:11 +0100

    Made it so that the transmutation stone item stack was taken from ee3 and removed mcmod.info as it wasn't working

--- common/com/github/RossRKK/EE3_Addons/lib/Reference.java
+++ common/com/github/RossRKK/EE3_Addons/lib/Reference.java
@@ -1,8 +1,15 @@
 package com.github.RossRKK.EE3_Addons.lib;
 
+import java.util.List;
+
+import com.pahimar.ee3.recipe.RecipesTransmutationStone;
+
+import net.minecraft.item.ItemStack;
+
 public class Reference
 {
     public static final String MOD_ID = "EE3_ADDONS";
     public static final String MOD_NAME = "Equivalent Exchange 3 Addons";
     public static final String VERSION = "Alpha";
+    public static final List<ItemStack> TRANSMUTATION_STONES = RecipesTransmutationStone.transmutationStones;
 }
--- common/com/github/RossRKK/EE3_Addons/recipes/CopperTransmute.java
+++ common/com/github/RossRKK/EE3_Addons/recipes/CopperTransmute.java
@@ -4,6 +4,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
+import com.github.RossRKK.EE3_Addons.lib.Reference;
 import com.pahimar.ee3.core.helper.RecipeHelper;
 import com.pahimar.ee3.item.ModItems;
 
@@ -12,16 +13,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CopperTransmute {
-    private static ItemStack philStone = new ItemStack(ModItems.philStone, 1,
-            OreDictionary.WILDCARD_VALUE);
-    private static ItemStack miniumStone = new ItemStack(ModItems.miniumStone,
-            1, OreDictionary.WILDCARD_VALUE);
-
-    public static List<ItemStack> transmutationStones = Arrays.asList(
-            miniumStone, philStone);
 
     public static void init() {
-        for (ItemStack stone : transmutationStones) {
+        for (ItemStack stone : Reference.TRANSMUTATION_STONES) {
             copper(stone);
             tin(stone);
             silver(stone);
