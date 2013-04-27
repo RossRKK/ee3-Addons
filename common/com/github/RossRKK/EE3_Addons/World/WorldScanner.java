ef07953e4a2529e8d98a193033d74077cba50dbf
Ross Kenneth Keslo <rosskelso@btinternet.com>
2013-04-27 12:51:53 +0100

    Added a numbers refrence files and the world scanner class

---
+++ common/com/github/RossRKK/EE3_Addons/World/WorldScanner.java
@@ -0,0 +1,30 @@
+package com.github.RossRKK.EE3_Addons.World;
+
+import com.github.RossRKK.EE3_Addons.lib.Numbers;
+
+import net.minecraft.world.World;
+
+public abstract class WorldScanner {
+    
+    World world;
+    
+    public void WorldScan(int x, int y, int z){
+        //calculate search box bounds
+        int searchStartX = x - Numbers.SEARCH_RADIUS;
+        int searchStartY = y - Numbers.SEARCH_RADIUS;
+        int searchStartZ = z - Numbers.SEARCH_RADIUS;
+        
+        int searchEndX = x + Numbers.SEARCH_RADIUS;
+        int searchEndY = y + Numbers.SEARCH_RADIUS;
+        int searchEndZ = z + Numbers.SEARCH_RADIUS;
+        
+        //search for the world
+        for (int a = searchStartX; a < searchEndX; a ++){
+            for (int b = searchStartY; b < searchEndY; b++){
+                for (int c = searchStartZ; c < searchEndZ; c++){
+                    int id = world.getBlockId(a, b, c);
+                }
+            }
+        }
+    }
+}
---
+++ common/com/github/RossRKK/EE3_Addons/lib/Numbers.java
@@ -0,0 +1,5 @@
+package com.github.RossRKK.EE3_Addons.lib;
+
+public class Numbers {
+    public static final int SEARCH_RADIUS = 3;
+}
